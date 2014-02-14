a1d34298ea8ef5a5173f176f408b4e8f266923ef
Florjan Bartol <florjan.bartol@gmail.com>
2014-02-14 11:28:43 +0100

    added .gitignore, helper html file

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,12 +1,4 @@
 var container = document.createElement('div');
 container.className = 'mb-container';
-container.style.cssText = '
-	width: 100%;
-		height: 100px;
-		position: fixed;
-		background-color: red;
-		top: 0;
-		z-index: 10000;
-		}
-	';
+container.style.cssText = 'width: 100%; height: 100px; position: fixed; background-color: red; top: 0; z-index: 10000;';
 document.body.appendChild(container);
