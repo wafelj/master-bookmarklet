364dabbb567a45690d3219d1f690338a57e55788
Florjan Bartol <florjan.bartol@gmail.com>
2014-02-14 17:05:34 +0100

    hackday presentation version

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,13 +1,54 @@
-// jsonp
-window.handler = function(data) {
-    console.log('got data:', data);
-};
+(function() {
+  if(document.getElementById('mb-container')) {
+    return;
+  }
 
-var script = document.createElement('script');
-script.src = 'http://localhost:3000/api/bookmarklets?callback=handler';
-document.head.appendChild(script);
+  var mbContainer = document.createElement('div');
+  mbContainer.id = 'mb-container';
+  mbContainer.style.cssText = 'padding-bottom: 30px; left: 0; right: 0; position: fixed; background-color: white; top: 0; z-index: 10000;';
+  mbContainer.innerHTML = '<div id="mb-header" style="height:50px;"><div id="mb-close" style="height: 50px;width: 50px;float: right; background-size: cover; background-image: url(http://static.freepik.com/free-photo/close-button-with-rounded-corners_318-9865.jpg);"></div></div>';
+  document.body.appendChild(mbContainer);
 
-var container = document.createElement('div');
-container.className = 'mb-container';
-container.style.cssText = 'width: 100%; height: 100px; position: fixed; background-color: red; top: 0; z-index: 10000;';
-document.body.appendChild(container);
+  var mbItemData;
+
+  var execute = function(e) {
+    var id = e.srcElement.getAttribute('data-id'),
+        data;
+
+    for(var i=0; i<mbItemData.length; i++) {
+      if(mbItemData[i]._id === id) {
+        eval(mbItemData[i].code);
+        break;
+      }
+    }
+  };
+
+  var close = function() {
+    document.getElementById('mb-close').removeEventListener("click", close, false);
+    document.body.removeChild(mbContainer);
+    mbContainer = undefined;
+    mbItemData = undefined;
+  };
+  document.getElementById('mb-close').addEventListener("click", close, false);
+
+  // jsonp
+  window.handler = function(data) {
+    for(var i=0; i<data.length; i++) {
+      var item = document.createElement('div');
+      //item.setAttribute('data-id', data[i]._id);
+      //item.className = 'mb-close';
+      item.innerHTML = '<img data-id="' + data[i]._id + '" src="' + data[i].thumbnailUrl + '" style="width: 80px; height: 80px; background-size: cover;" /><span>' + data[i].name + '</span>';
+      item.style.cssText = 'height: 100px; width: 80px; padding: 10px;';
+      mbContainer.appendChild(item);
+
+      item.addEventListener("click", execute, false);
+
+      mbItemData = data;
+    }
+  };
+
+  var script = document.createElement('script');
+  script.src = 'http://localhost:3000/api/bookmarklets?callback=handler';
+  document.head.appendChild(script);
+})();
+
--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -1,24 +1,32 @@
 Bookmarklets = new Meteor.Collection('bookmarklets');
 
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to dashboard.";
-  };
-
   Template.add.events({
     'click input.add-bookmarklet' : function () {
       var name = document.getElementById('bookmarklet-name').value;
       var code = document.getElementById('bookmarklet-code').value;
+      var thumbnailUrl = document.getElementById('bookmarklet-thumbnail-url').value;
+
+      if(!name || !code || !thumbnailUrl) {
+        return;
+      }
 
       Bookmarklets.insert({
+        'userId': Meteor.userId(),
         'name' : name,
-        'code' : code
+        'code' : code,
+        'thumbnailUrl': thumbnailUrl,
       });
     }
   });
 
+    
+  Template.bookmarklets.empty = function () {
+    return Bookmarklets.find({'userId': Meteor.userId()}).count === 0;
+  };
+
   Template.bookmarklets.items = function () {
-    return Bookmarklets.find({});
+    return Bookmarklets.find({'userId': Meteor.userId()});
   };
 }
 
