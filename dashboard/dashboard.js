17c69569267025324cc6b89c48c82ddb73c12147
Florjan Bartol <florjan.bartol@gmail.com>
2014-02-16 16:27:34 +0100

    improved dashboard, added CRUD operations, nicer design

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -1,36 +1,68 @@
 Bookmarklets = new Meteor.Collection('bookmarklets');
 
 if (Meteor.isClient) {
-  Template.add.events({
-    'click input.add-bookmarklet' : function () {
-      var name = document.getElementById('bookmarklet-name').value;
-      var code = document.getElementById('bookmarklet-code').value;
-      var thumbnailUrl = document.getElementById('bookmarklet-thumbnail-url').value;
-
-      if(!name || !code || !thumbnailUrl) {
-        return;
-      }
-
-      Bookmarklets.insert({
-        'userId': Meteor.userId(),
-        'name' : name,
-        'code' : code,
-        'thumbnailUrl': thumbnailUrl,
-      });
-    }
-  });
-
-    
-  Template.bookmarklets.empty = function () {
-    return Bookmarklets.find({'userId': Meteor.userId()}).count === 0;
-  };
-
   Template.bookmarklets.items = function () {
     return Bookmarklets.find({'userId': Meteor.userId()});
   };
+
+  Template.bookmarklets.events({
+    'click .list-group-item' : function () {
+      Session.set('currentId', this._id);
+      $('#dialog').modal('show');
+    }
+  });
+
+  Template.dialog.item = function() {
+    return Bookmarklets.findOne(Session.get('currentId'));
+  };
+  
+  Template.dialog.events({
+    'click button#dialog-save' : function () {
+      // collect form data
+      var form = {};
+      $.each($('#dialog-form').serializeArray(), function() {
+          form[this.name] = this.value;
+      });
+
+      if(form.name && form.code) {
+        Meteor.call('upsert', form.id, {
+          'userId': Meteor.userId(),
+          'name' : form.name,
+          'description': form.description,
+          'thumbnailUrl': form.thumbnailUrl,
+          'code' : form.code,
+        });
+
+        $('#dialog').modal('hide');
+      }
+    },
+    'click button#dialog-delete' : function () {
+      var id = $('#dialog-form input[name=id]').val();
+
+      $('#dialog').one('hidden.bs.modal', function () {
+        Meteor.call('delete', id);
+      })
+      
+      $('#dialog').modal('hide');
+    }
+  }); 
 }
 
 if (Meteor.isServer) {
+  Meteor.methods({
+    upsert: function (id, doc) {
+      if(doc.userId === Meteor.userId() && doc.name && doc.code) {
+        Bookmarklets.upsert(id, doc);
+      }
+    }, 
+
+    delete: function (id) {
+      if(Bookmarklets.findOne(id).userId === Meteor.userId()) {
+        Bookmarklets.remove(id);
+      }
+    }
+  });
+
   Meteor.startup(function () {
     collectionApi = new CollectionAPI({
       authToken: undefined,
